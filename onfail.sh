c829d0cdcc49c5432f8e23c90811eba42bd57868
Clint Sharp <clint@taktak.io>
2018-01-23 12:25:45 -0800

    Changing to codeshipdemo space

--- onfail.sh
+++ onfail.sh
@@ -1,6 +1,6 @@
 set -x
 mkdir -p /build
-diag mkds --space codeship --name "${CI_REPO_NAME:-missing_repo_name} -- ${CI_BUILD_ID:-missing_build_id}" --desc "${CI_COMMIT_MESSAGE:-missing_commit_message}" -j > /build/dataset.json
+diag mkds --space codeshipdemo --name "${CI_REPO_NAME:-missing_repo_name} -- ${CI_BUILD_ID:-missing_build_id}" --desc "${CI_COMMIT_MESSAGE:-missing_commit_message}" -j > /build/dataset.json
 cat /build/dataset.json | jq -r '. | "d/\(.id.space_id)/\(.id.item_id)"' > /build/dataset_id
 diag cp /build/logs --name logs.zip $(cat /build/dataset_id)
 diag cp /src --name source.zip --gitignore $(cat /build/dataset_id)
